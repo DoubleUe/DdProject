11928802201b7e72ef5cf62224387f152032e1eb
DoubleUe <verygoodd21@gmail.com>
2026-03-31 11:50:05 +0900

    베이스 애니메이션에 변수값들 작업.

--- Source/DdProject/Classes/Animation/DdBaseAnimInstance.cpp
+++ Source/DdProject/Classes/Animation/DdBaseAnimInstance.cpp
@@ -1,1 +1,34 @@
 #include "Animation/DdBaseAnimInstance.h"
+
+#include "Character/DdBaseCharacter.h"
+#include "CharacterTrajectoryComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UDdBaseAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	CacheOwnerComponents();
+}
+
+void UDdBaseAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	CacheOwnerComponents();
+}
+
+void UDdBaseAnimInstance::CacheOwnerComponents()
+{
+	ADdBaseCharacter* BaseCharacter = Cast<ADdBaseCharacter>(TryGetPawnOwner());
+
+	if (!BaseCharacter)
+	{
+		TrajectoryComponent = nullptr;
+		CharacterMovementComponent = nullptr;
+		return;
+	}
+
+	TrajectoryComponent = BaseCharacter->GetTrajectoryComponent();
+	CharacterMovementComponent = BaseCharacter->GetCharacterMovement();
+}
--- Source/DdProject/Classes/Animation/DdBaseAnimInstance.h
+++ Source/DdProject/Classes/Animation/DdBaseAnimInstance.h
@@ -4,8 +4,25 @@
 #include "Animation/AnimInstance.h"
 #include "DdBaseAnimInstance.generated.h"
 
+class UCharacterMovementComponent;
+class UCharacterTrajectoryComponent;
+
 UCLASS(Blueprintable, BlueprintType)
 class DDPROJECT_API UDdBaseAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "Character|Components")
+	TObjectPtr<UCharacterTrajectoryComponent> TrajectoryComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "Character|Components")
+	TObjectPtr<UCharacterMovementComponent> CharacterMovementComponent = nullptr;
+
+private:
+	void CacheOwnerComponents();
 };
