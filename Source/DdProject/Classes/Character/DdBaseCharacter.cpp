4a02463abfed41e7743b88929e6d8cb6a7528bf5
DoubleUe <verygoodd21@gmail.com>
2026-03-31 13:57:10 +0900

    무기 장착

--- Source/DdProject/Classes/Character/DdBaseCharacter.cpp
+++ Source/DdProject/Classes/Character/DdBaseCharacter.cpp
@@ -1,11 +1,29 @@
 #include "DdBaseCharacter.h"
 
 #include "CharacterTrajectoryComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 ADdBaseCharacter::ADdBaseCharacter()
 {
 	TrajectoryComponent = CreateDefaultSubobject<UCharacterTrajectoryComponent>(TEXT("TrajectoryComponent"));
+	EquippedStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("EquippedStaticMeshComponent"));
+
+	if (EquippedStaticMeshComponent != nullptr)
+	{
+		EquippedStaticMeshComponent->SetupAttachment(GetMesh());
+		EquippedStaticMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		EquippedStaticMeshComponent->SetGenerateOverlapEvents(false);
+		EquippedStaticMeshComponent->SetCanEverAffectNavigation(false);
+	}
+}
+
+void ADdBaseCharacter::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	InitializeEquippedStaticMeshAttachment();
 }
 
 UCharacterTrajectoryComponent* ADdBaseCharacter::GetTrajectoryComponent() const
@@ -37,4 +55,45 @@ void ADdBaseCharacter::ToggleWalkSpeed()
 
 	bUseSlowWalkSpeed = !bUseSlowWalkSpeed;
 	CharacterMovementComponent->MaxWalkSpeed = bUseSlowWalkSpeed ? SlowWalkSpeed : NormalWalkSpeed;
+}
+
+void ADdBaseCharacter::InitializeEquippedStaticMeshAttachment()
+{
+	USkeletalMeshComponent* CharacterMeshComponent = GetMesh();
+	if (CharacterMeshComponent == nullptr || EquippedStaticMeshComponent == nullptr)
+	{
+		return;
+	}
+
+	if (!CharacterMeshComponent->DoesSocketExist(EquippedStaticMeshParentSocketName))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Missing parent socket '%s' on character mesh."), *GetName(), *EquippedStaticMeshParentSocketName.ToString());
+		EquippedStaticMeshComponent->AttachToComponent(CharacterMeshComponent, FAttachmentTransformRules::KeepRelativeTransform);
+		EquippedStaticMeshComponent->SetRelativeTransform(FTransform::Identity);
+		return;
+	}
+
+	EquippedStaticMeshComponent->AttachToComponent(
+		CharacterMeshComponent,
+		FAttachmentTransformRules::KeepRelativeTransform,
+		EquippedStaticMeshParentSocketName);
+
+	if (EquippedStaticMeshComponent->GetStaticMesh() == nullptr)
+	{
+		EquippedStaticMeshComponent->SetRelativeTransform(FTransform::Identity);
+		return;
+	}
+
+	if (!EquippedStaticMeshComponent->DoesSocketExist(EquippedStaticMeshChildSocketName))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Missing child socket '%s' on equipped static mesh."), *GetName(), *EquippedStaticMeshChildSocketName.ToString());
+		EquippedStaticMeshComponent->SetRelativeTransform(FTransform::Identity);
+		return;
+	}
+
+	const FTransform ChildSocketComponentTransform = EquippedStaticMeshComponent->GetSocketTransform(
+		EquippedStaticMeshChildSocketName,
+		ERelativeTransformSpace::RTS_Component);
+
+	EquippedStaticMeshComponent->SetRelativeTransform(ChildSocketComponentTransform.Inverse());
 }
--- Source/DdProject/Classes/Character/DdBaseCharacter.h
+++ Source/DdProject/Classes/Character/DdBaseCharacter.h
@@ -5,6 +5,7 @@
 #include "DdBaseCharacter.generated.h"
 
 class UCharacterTrajectoryComponent;
+class UStaticMeshComponent;
 
 UCLASS(Abstract)
 class DDPROJECT_API ADdBaseCharacter : public ACharacter
@@ -27,14 +28,27 @@ public:
 	virtual void SetAttackInputBlocked(bool bBlocked) PURE_VIRTUAL(ADdBaseCharacter::SetAttackInputBlocked, );
 
 protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	void InitializeEquippedStaticMeshAttachment();
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trajectory", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UCharacterTrajectoryComponent> TrajectoryComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Equipment", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> EquippedStaticMeshComponent;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float NormalWalkSpeed = 500.0f;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float SlowWalkSpeed = 250.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Equipment", meta = (AllowPrivateAccess = "true"))
+	FName EquippedStaticMeshParentSocketName = TEXT("hand_r_weapon");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Equipment", meta = (AllowPrivateAccess = "true"))
+	FName EquippedStaticMeshChildSocketName = TEXT("hand");
 
 	UPROPERTY(Transient)
 	bool bUseSlowWalkSpeed = false;
