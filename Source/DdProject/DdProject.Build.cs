6fbc3d3c75758a8ef093b1ced79203ad060c2db7
DoubleUe <verygoodd21@gmail.com>
2026-03-31 16:23:27 +0900

    피투피 작업

--- Source/DdProject/DdProject.Build.cs
+++ Source/DdProject/DdProject.Build.cs
@@ -11,16 +11,13 @@ public class DdProject : ModuleRules
 		PublicIncludePaths.Add(ModuleDirectory + "/Classes");
 		PrivateIncludePaths.Add(ModuleDirectory + "/Classes");
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "DeveloperSettings", "AIModule", "NavigationSystem", "GameplayTasks", "MotionTrajectory" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "DeveloperSettings", "AIModule", "NavigationSystem", "GameplayTasks", "MotionTrajectory", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
 	}
 }
